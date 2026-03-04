c0ca986af8159aaa04bd9d0dcc10666c70dbef9b
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-03-04 01:32:58 +0100

    Correcciones en la rutas y vistas

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('partials.login');
+        return view('auth.login');
     }
 
     /**
