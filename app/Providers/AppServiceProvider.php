070c63f92d204aac1f8834cdc0dbebdd045cae4a
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-03-04 01:08:20 +0100

    Hacer posible el funcionamiento a travez de https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
         Blade::if("admin", function(){
             return Auth::check() && Auth::user()->isAdmin();
         });
+
+        // Comenta esta linea si usas un entorno de desarrollo local sin HTTPS
+        URL::forceScheme('https');
     }
 }
